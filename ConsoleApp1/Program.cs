113fd0bc0e21901b748ea567d757914b0488c0ea
MarkRockord <markrokfild@gmail.com>
2024-10-04 14:53:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            Console.WriteLine("ENTER X AND Y");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+            int p = x * y;
+            Console.WriteLine(p);
+            Console.ReadKey();
+        }
+    }
+}
